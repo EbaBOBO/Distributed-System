ad47819d8137e6b4414b8a0ce179657a62c18b18
Blue-Clipper <zichanggao54@gmail.com>
2023-05-02 16:58:42 -0400

    Add updateCommitIndex func

--- replication/raft/request_send.go
+++ replication/raft/request_send.go
@@ -14,6 +14,7 @@ func sendAppendEntries(rn *RaftNode, init bool, higherTermChan chan uint64) {
 		rn.log.Printf("Leader send AppendEntries to %v, lastLogIdx %v,commitIdx %v, nextIdx %v, matchIdx %v", nd, rn.LastLogIndex(), rn.commitIndex, rn.nextIndex, rn.matchIndex)
 		// If last log index ≥ nextIndex for a follower: send AppendEntries RPC with log entries starting at nextIndex
 		go func(nodeId uint64) {
+			defer updateCommitIndex(rn)
 			defer rn.log.Printf("Leader after sending AppendEntries to %v, lastLogIdx %v, commitIdx %v, nextIdx %v, matchIdx %v", nodeId, rn.LastLogIndex(), rn.commitIndex, rn.nextIndex, rn.matchIndex)
 			rn.leaderMu.Lock()
 			nextIdx := rn.nextIndex[nodeId]
@@ -65,7 +66,7 @@ func sendAppendEntries(rn *RaftNode, init bool, higherTermChan chan uint64) {
 			}
 			// Update nextIndex and matchIndex for the follower if successful
 			rn.leaderMu.Lock()
-
+			defer rn.leaderMu.Unlock()
 			if rn.nextIndex[nodeId] < 1 {
 				panic("nextIndex < 1")
 			}
@@ -78,43 +79,46 @@ func sendAppendEntries(rn *RaftNode, init bool, higherTermChan chan uint64) {
 				if rn.nextIndex[nodeId] > 1 {
 					rn.nextIndex[nodeId] -= 1
 				}
-				rn.leaderMu.Unlock()
 				return
 			}
-			rn.leaderMu.Unlock()
-			// If there exists an N such that N > commitIndex, a majority
-			// of matchIndex[i] ≥ N, and log[N].term == currentTerm:
-			// set commitIndex = N
-			N := rn.commitIndex
-			newIdx := N
-			rn.log.Printf("nextIdx %v", rn.nextIndex)
-			rn.log.Printf("matchIdx %v", rn.matchIndex)
-			rn.log.Printf("commitIdx %v", rn.commitIndex)
-			for {
-				N += 1
-				cnt := 0
-				for _, v := range rn.matchIndex {
-					if v >= N {
-						cnt++
-					}
-				}
-				if cnt >= (len(rn.node.PeerNodes)/2) && rn.GetLog(N) != nil && rn.GetLog(N).Term == rn.GetCurrentTerm() {
-					newIdx = N
-					continue
-				} else {
-					break
-				}
-			}
-			rn.commitIndex = newIdx
-			rn.log.Printf("commitIdx after %v", rn.commitIndex)
-			for rn.commitIndex > rn.lastApplied {
-				rn.lastApplied++
-				if rn.GetLog(rn.lastApplied).Data == nil {
-					continue
-				}
-				rn.commitC <- (*commit)(&rn.GetLog(rn.lastApplied).Data)
-			}
 		}(nd)
+	}
+}
+
+func updateCommitIndex(rn *RaftNode) {
+	rn.leaderMu.Lock()
+	defer rn.leaderMu.Unlock()
+	// If there exists an N such that N > commitIndex, a majority
+	// of matchIndex[i] ≥ N, and log[N].term == currentTerm:
+	// set commitIndex = N
+	N := rn.commitIndex
+	newIdx := N
+	rn.log.Printf("nextIdx %v", rn.nextIndex)
+	rn.log.Printf("matchIdx %v", rn.matchIndex)
+	rn.log.Printf("commitIdx %v", rn.commitIndex)
+	for {
+		N += 1
+		cnt := 0
+		for _, v := range rn.matchIndex {
+			if v >= N {
+				cnt++
+			}
+		}
+		if cnt >= (len(rn.node.PeerNodes)/2) && rn.GetLog(N) != nil && rn.GetLog(N).Term == rn.GetCurrentTerm() {
+			newIdx = N
+			continue
+		} else {
+			break
+		}
+	}
+	rn.commitIndex = newIdx
+	rn.log.Printf("commitIdx after %v", rn.commitIndex)
+	for rn.commitIndex > rn.lastApplied {
+		rn.lastApplied++
+		if rn.GetLog(rn.lastApplied).Data == nil {
+			continue
+		}
+		rn.commitC <- (*commit)(&rn.GetLog(rn.lastApplied).Data)
 	}
 }
 
